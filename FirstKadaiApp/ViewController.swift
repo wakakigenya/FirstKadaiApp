51e00b15e2ae7aa587e64787648d5599184e06f1
genya.wakaki <wakawakawaka44@gmail.com>
2020-12-22 09:48:18 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 若木弦也 on 2020/12/22.
+//  Copyright © 2020 genya.wakaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
